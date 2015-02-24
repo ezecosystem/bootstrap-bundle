b497fba85b9a4af5f7f5f9671f5fd82eabe9d0f8
dennispoppe <dennis@xrow.de>
2015-02-24 12:22:40 +0100

    load settings from yml to ConfigResolver

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,20 +2,42 @@
 
 namespace xrow\bootstrapBundle\DependencyInjection;
 
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\Configuration as SiteAccessConfiguration;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
  * This is the class that validates and merges configuration from your app/config files
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
  */
-class Configuration implements ConfigurationInterface
+class Configuration extends SiteAccessConfiguration
 {
     /**
      * {@inheritdoc}
      */
     public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root( 'acme_demo' );
+
+        // $systemNode will then be the root of siteaccess aware settings.
+        $systemNode = $this->generateScopeBaseNode( $rootNode );
+        $systemNode
+        ->scalarNode( 'hello' )->isRequired()->end()
+        ->arrayNode( 'foo_setting' )
+        ->children()
+        ->scalarNode( "foo" )->end()
+        ->scalarNode( "some" )->end()
+        ->integerNode( "an_integer" )->end()
+        ->booleanNode( "enabled" )->end()
+        ->end()
+        ->end();
+
+        return $treeBuilder;
+    }
+
+    /*public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('xrowbootstrap');
@@ -25,5 +47,5 @@ class Configuration implements ConfigurationInterface
         // more information on that topic.
 
         return $treeBuilder;
-    }
+    }*/
 }
--- DependencyInjection/xrowbootstrapExtension.php
+++ DependencyInjection/xrowbootstrapExtension.php
@@ -2,6 +2,8 @@
 
 namespace xrow\bootstrapBundle\DependencyInjection;
 
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ConfigurationProcessor;
+use eZ\Bundle\EzPublishCoreBundle\DependencyInjection\Configuration\SiteAccessAware\ContextualizerInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
@@ -17,12 +19,40 @@ class xrowbootstrapExtension extends Extension
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+        public function load( array $configs, ContainerBuilder $container )
+        {
+            $configuration = $this->getConfiguration( $configs, $container );
+            $config = $this->processConfiguration( $configuration, $configs );
+    
+            $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__.'/../Resources/config' ) );
+            $loader->load( 'default_settings.yml' );
+    
+            // "acme_demo" will be the namespace as used in ConfigResolver format.
+            $processor = new ConfigurationProcessor( $container, 'acme_demo' );
+            $processor->mapConfig(
+                $config,
+                // Any kind of callable can be used here.
+                // It will be called for each declared scope/SiteAccess.
+                function ( $scopeSettings, $currentScope, ContextualizerInterface $contextualizer )
+                {
+                    // Will map "hello" setting to "acme_demo.<$currentScope>.hello" container parameter
+                    // It will then be possible to retrieve this parameter through ConfigResolver in the application code:
+                    // $helloSetting = $configResolver->getParameter( 'hello', 'acme_demo' );
+                    $contextualizer->setContextualParameter( 'hello', $currentScope, $scopeSettings['hello'] );
+                }
+            );
+    
+            // Now map "foo_setting" and ensure keys defined for "my_siteaccess" overrides the one for "my_siteaccess_group"
+            // It is done outside the closure as it is needed only once.
+            $processor->mapConfigArray( 'foo_setting', $config );
+        }
+
+    /*public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-    }
+    }*/
 }
