3d5e869f57ab339fb1256f2de5cb0ad9dfce75ed
xrowkristina <kristina@xrow.de>
2015-06-18 16:15:26 +0200

    bugfixing

--- DependencyInjection/xrowbootstrapExtension.php
+++ DependencyInjection/xrowbootstrapExtension.php
@@ -2,43 +2,49 @@
 
 namespace xrow\bootstrapBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\DependencyInjection\Alias;
-use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class xrowbootstrapExtension extends Extension
+class xrowbootstrapExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = $this->getConfiguration( $configs, $container );
-        $config = $this->processConfiguration( $configuration, $configs );
+
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('config.yml');
-        
-        #die(var_dump($config));
+        $loader->load('services.yml');
+        // Overwrite data
         if (null !== $config['options']['show_navigation_identifier']) {
-            $container->setParameter('xrowbootstrap.options.show_navigation_identifier', $config['options']['show_navigation_identifier']);
+            $container->setParameter('xrowbootstrap.show_navigation_identifier', $config['options']['show_navigation_identifier']);
         }
     }
 
     /**
-     * {@inheritdoc}
+     * Loads bootstrap configuration
+     *
+     * @param ContainerBuilder $container
      */
-    public function getAlias()
+    public function prepend( ContainerBuilder $container )
     {
-        return 'xrowbootstrap';
+        $configFile = __DIR__ . '/../Resources/config/xrowbootstrap.yml';
+        $config = Yaml::parse( file_get_contents( $configFile ) );
+
+        $container->prependExtensionConfig( 'xrowbootstrap', $config );
+        $container->addResource( new FileResource( $configFile ) );
     }
 }
