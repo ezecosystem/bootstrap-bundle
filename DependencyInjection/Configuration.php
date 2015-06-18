bb75ed17d944668eccc451f30b292b59b1720ce5
dennispoppe <dennis@xrow.de>
2015-06-18 14:04:16 +0200

    fixed wrong namespace

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace xrow\bootstrapBundle\DependencyInjection
+namespace xrow\bootstrapBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
