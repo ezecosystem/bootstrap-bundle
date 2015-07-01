c802f6eabadfef8901c1b92d66229dbc3b2246c3
xrowkristina <kristina@xrow.de>
2015-07-01 15:17:37 +0200

    bugfixing for route test/html

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -9,7 +9,6 @@ use Knp\Menu\Matcher\Matcher;
 use Knp\Menu\MenuFactory;
 use Knp\Menu\Renderer\ListRendPageclass;
 
-
 class PageController extends Controller
 {
     public function indexAction()
@@ -23,7 +22,7 @@ class PageController extends Controller
         $menu['Kräuter']->addChild('Suppengrün', array('uri' => '#'));
         $menu['Kräuter']->addChild('Edelwurz', array('uri' => '#'));
         $menu['Kräuter']->addChild('Snuff', array('uri' => '#'));
-        #return $this->render('xrowbootstrapBundle:Default:index.html.twig');
+
         return $this->render('xrowbootstrapBundle:Default:test_html.html.twig',array('menu' => $menu));
     }
 
@@ -38,19 +37,7 @@ class PageController extends Controller
         $menu['Kräuter']->addChild('Suppengrün', array('uri' => '#'));
         $menu['Kräuter']->addChild('Edelwurz', array('uri' => '#'));
         $menu['Kräuter']->addChild('Snuff', array('uri' => '#'));
-        #return $this->render('xrowbootstrapBundle:Default:index.html.twig');
+
         return $this->render('xrowbootstrapBundle:Default:test_bootstrap.html.twig',array('menu' => $menu));
     }
-
-    public function wuvAction()
-    {
-        return $this->render('xrowbootstrapBundle:Default:test_elemente_wuv.html.twig');
-    }
-    
-    public function wuv_homeAction()
-    {
-        return $this->render('xrowbootstrapBundle:Default:test_home_wuv.html.twig');
-    }
 }
-
-
