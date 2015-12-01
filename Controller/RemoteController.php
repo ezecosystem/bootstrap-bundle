f6c833d096c2216410b3e9b2b3d5cd64ffa69102
xrowkristina <kristina@xrow.de>
2015-12-01 14:26:04 +0100

    optimzed cod

--- Controller/RemoteController.php
+++ Controller/RemoteController.php
@@ -15,6 +15,9 @@ use eZ\Publish\Core\Base\Exceptions\NotFoundException;
 class RemoteController extends Controller
 {
     /**
+     * Replace body content with template "xrowbootstrapBundle:full:remotecontent.html.twig": placeholder <!--CONTENT-->
+     * Replace all href, src, action and maybe content paths with absolute paths
+     * 
      * @Route("/remote/content/{viewType}/{locationId}", requirements={"viewType" = "\w+", "locationId" = "\d+"}, defaults={"viewType" = "full"})
      */
     public function getContentAction(Request $request, $viewType, $locationId)
@@ -33,30 +36,30 @@ class RemoteController extends Controller
         if ($location->invisible) {
             throw new NotFoundHttpException("Location #$locationId cannot be displayed as it is flagged as invisible.");
         }
-        $viewManager = $this->container->get('ezpublish.view_manager');
-        $allLocationViewProviders = $viewManager->getAllLocationViewProviders();
-        foreach ($allLocationViewProviders as $viewProvider) {
-            $view = $viewProvider->getView($location, $viewType);
-            if ($view instanceof \eZ\Publish\Core\MVC\Symfony\View\ContentViewInterface) {
-                $templateIdentifier = $view->getTemplateIdentifier();
-                break;
-            }
-        }
-        if (isset($templateIdentifier)) {
+        $locationViewMatcher = $this->container->get('ezpublish.location_view.matcher_factory');
+        $match = $locationViewMatcher->match($location, $viewType);
+        if (isset($match['template']) || isset($match['controller'])) {
+            $templateIdentifier = isset($match['template']) ? $match['template'] : $match['controller'];
             if (strpos($templateIdentifier, '::') !== false)
                 $templateIdentifierArray = explode('::', $templateIdentifier);
             elseif (strpos($templateIdentifier, ':') !== false)
                 $templateIdentifierArray = explode(':', $templateIdentifier);
-            $designBundle = $templateIdentifierArray[0];
-            $response = $this->render('xrowbootstrapBundle:full:remotecontent.html.twig', array('designBundle' => $designBundle));
-            $content = $response->getContent();
-            $content = preg_replace( '#(href|src|action)="(?!(|http:|https:)//)([^:"]*)("|(?:(?:%20|\s|\+)[^"]*"))#', '$1="//' . $host . '$2$3"', $content );
-            $content = preg_replace( '#url\((?!\s*[\'"]?(?:https?:)?//)\s*([\'"])?#', "url($1//{$host}", $content );
-            /*if (preg_match('/\<meta name="msapplication-TileImage" content="[\w\d.\?\/]*"\>/', $content, $matches)) {
-                die(var_dump($matches));
-            }*/
-            $response->setContent($content);
-            return $response;
+            if (isset($templateIdentifierArray[0])) {
+                $designBundle = $templateIdentifierArray[0];
+                $response = $this->render('xrowbootstrapBundle:full:remotecontent.html.twig', array('designBundle' => $designBundle));
+                $content = $response->getContent();
+                $content = preg_replace('#(href|src|action)="(?!(|http:|https:)//)([^:"]*)("|(?:(?:%20|\s|\+)[^"]*"))#', '$1="//' . $host . '$2$3"', $content);
+                $content = preg_replace('#url\((?!\s*[\'"]?(?:https?:)?//)\s*([\'"])?#', "url($1//{$host}", $content);
+                if (preg_match('#<meta name="msapplication-TileImage" content="(?!(|http:|https:)//)([^:"]*)("|(?:(?:%20|\s|\+)[^"]*"))#', $content)) {
+                    $content = preg_replace('#<meta name="msapplication-TileImage" content="(?!(|http:|https:)//)([^:"]*)("|(?:(?:%20|\s|\+)[^"]*"))#', '<meta name="msapplication-TileImage" content="//' . $host . '$2$3"', $content);
+                }
+                $response->setContent($content);
+                return $response;
+            }
+        }
+        else {
+            $contentType = $repository->getContentTypeService()->loadContentType($location->contentInfo->contentTypeId)->identifier;
+            return new Response('Please set a template or controller for contentType "'.$contentType.'" in your YAML.', 500);
         }
     }
 }
