3436ff3a2b9278b6e767a28e0780d39cb81b3c39
xrowkristina <kristina@xrow.de>
2016-03-04 13:50:03 +0100

    Bugfixing for not existing routes

--- Controller/RedirectingController.php
+++ Controller/RedirectingController.php
@@ -4,6 +4,7 @@ namespace xrow\bootstrapBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class RedirectingController extends Controller
 {
@@ -11,7 +12,19 @@ class RedirectingController extends Controller
     {
         $pathInfo = $request->getPathInfo();
         $requestUri = $request->getRequestUri();
-
+        // For not knowen routes. Without this we get a loop.
+        $checkPathInfo = str_replace($pathInfo, rtrim($pathInfo, ' /'), $pathInfo);
+        $router = $this->container->get('router');
+        $found = false;
+        foreach ($router->getRouteCollection()->all() as $name => $route) {
+            if ($checkPathInfo == $route->getPath()) {
+                $found = true;
+                break;
+            }
+        }
+        if (!$found) {
+            throw new NotFoundHttpException();
+        }
         $url = str_replace($pathInfo, rtrim($pathInfo, ' /'), $requestUri);
         return $this->redirect($url, 301);
     }
