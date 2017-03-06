7a3ebe72bacdaa1c315da50eb1446c6c77476203
Dominik Eberlein <dominik@xrow.de>
2017-03-06 11:55:55 +0100

    new cache purger - overwrite old service

---
+++ Components/Cache/PurgeClient.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace xrow\bootstrapBundle\Components\Cache;
+use eZ\Publish\Core\MVC\Symfony\Cache\Http\FOSPurgeClient;
+
+class PurgeClient extends FOSPurgeClient
+{
+    public function purgeAll()
+    {
+        
+    }
+}
